8a5eb6abd03eff24e43334545f0b573682ff6f44
Tomás Costa <tomasc98@live.com>
2023-12-30 10:57:05 -0300

    feat(login): login authentication added

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -15,5 +17,42 @@ class LoginController extends Controller
     public function view() : View {
 
         return view('auth.login');
+    }
+
+
+    /**
+     * Handle an authentication attempt.
+     * 
+     * @param \Illuminate\Http\Request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function login(Request $request) : RedirectResponse{
+
+        // Validate the incoming request data.
+        $credentials = $request->validateWithBag('login', [
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        // Attempt to authenticate the user using the provided credentials.
+        if(Auth::Attempt($credentials, false)) {
+
+            // Regenerate the session.
+            $request->session()->regenerate();
+
+            return to_route('home');
+        }
+
+        /**
+         * If authentication failed, invalidate the session and regenerate 
+         * the CSRF token.
+         */
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        session()->flash('problem', 'Error al iniciar sesión, correo o contraseña incorrecta');
+
+        // Redirect back to the login page with the error message.
+        return to_route('auth.login');
     }
 }
