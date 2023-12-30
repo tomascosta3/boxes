b9f05267639036b3a35260d214c4d9bdc8d682eb
Tomás Costa <tomasc98@live.com>
2023-12-30 10:26:04 -0300

    feat(logins): migration updated

--- database/migrations/2023_12_30_131646_create_logins_table.php
+++ database/migrations/2023_12_30_131646_create_logins_table.php
@@ -12,7 +12,36 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('logins', function (Blueprint $table) {
+
             $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            
+            $table->string('verification_code')->nullable();
+            
+            $table->timestamp('verification_code_issue_date')->nullable();
+            
+            $table->timestamp('verification_code_expiration_date')->nullable();
+
+            $table->boolean('forgot_password')->default(false);
+
+            $table->string('renovation_code')->nullable();
+
+            $table->timestamp('renovation_date')->nullable();
+
+            $table->timestamp('renovation_code_issue_date')->nullable();
+
+            $table->timestamp('renovation_code_expiration_date')->nullable();
+
+            $table->timestamp('last_login_date')->nullable();
+
+            $table->boolean('active')->default(true);
+
+            $table->boolean('was_imported')->default(false);
+
+            $table->boolean('first_login')->default(true);
+
             $table->timestamps();
         });
     }
