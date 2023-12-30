d42db16c49dace84ce34d07a409c39bec27d90de
Tomás Costa <tomasc98@live.com>
2023-12-30 10:33:18 -0300

    feat(logins): model created

---
+++ app/Models/Login.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Login extends Model
+{
+    use HasFactory;
+
+    protected $table = 'logins';
+
+    protected $fillable = [
+        'user_id',
+        'verification_code',
+        'verification_code_issue_date',
+        'verification_code_expiration_date',
+        'forgot_password',
+        'renovation_code',
+        'renovation_date',
+        'renovation_code_issue_date',
+        'renovation_code_expiration_date',
+        'last_login_date',
+        'active',
+        'was_imported',
+        'first_login',
+    ];
+
+    protected $hidden = [
+        'verification_code',
+        'renovation_code'
+    ];
+
+    /**
+     * Returns the user to whom the login belongs.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user() {
+
+        return $this->belongsTo(User::class);
+    }
+}
