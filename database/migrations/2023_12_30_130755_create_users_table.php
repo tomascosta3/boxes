fc0b7a0f121b4e363d126a28f8c5132d1f83e75a
Tomás Costa <tomasc98@live.com>
2024-01-03 09:57:55 -0300

    fix(user migration): nullable attributes fixed

--- database/migrations/2023_12_30_130755_create_users_table.php
+++ database/migrations/2023_12_30_130755_create_users_table.php
@@ -15,11 +15,11 @@ return new class extends Migration
 
             $table->id();
 
-            $table->string('first_name')->nullable();
+            $table->string('first_name');
 
-            $table->string('last_name')->nullable();
+            $table->string('last_name');
 
-            $table->string('phone_number')->nullable();
+            $table->string('phone_number');
 
             $table->string('email');
 
