59a87bfc4842cc23cb36d26e4ae7938e51f11b71
Tomas Costa <tomasc98@live.com>
2024-03-07 20:49:07 -0300

    fix(home): problem session centered

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -8,8 +8,8 @@
 
     @if (session('problem') != null)
 
-        <div class="columns is-centered is-vcentered">
-            <div class="column is-11">
+        <div class="columns is-centered is-vcentered mt-6">
+            <div class="column is-5">
                 <div class="notification is-danger">
                     <p class="has-text-centered">{{ session('problem') }}</p>
                 </div>
