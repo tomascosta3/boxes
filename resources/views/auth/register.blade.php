2e97a632965c571846e0f2da2f1d23b31295db43
Tomas Costa <tomasc98@live.com>
2024-03-08 15:34:33 -0300

    fix(register): add view password

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -90,14 +90,27 @@
     
                                 <div class="field">
                                     <label class="label" for="password">Contraseña</label>
-                                    <div class="control has-icons-left has-icons-right">
-                                        <input class="input" type="password" name="password" id="password" placeholder="***********">
-                                        <span class="icon is-small is-left">
-                                            <i class="bx bx-key"></i>
-                                        </span>
-                                        <span class="icon is-small is-right">
-                                            <i class='bx bx-error-circle'></i>
-                                        </span>
+                                    <div class="field is-grouped">
+                                        <div class="field-body">
+                                            <div class="field has-addons">
+                                                <div class="control is-expanded has-icons-left has-icons-right">
+                                                    <input class="input" type="password" name="password" id="password" placeholder="***********">
+                                                    <span class="icon is-small is-left">
+                                                        <i class="bx bx-key"></i>
+                                                    </span>
+                                                    <span class="icon is-small is-right">
+                                                        <i class='bx bx-error-circle'></i>
+                                                    </span>
+                                                </div>
+                                                <div class="control">
+                                                    <a class="button" id="toggle-password">
+                                                        <span class="icon is-small">
+                                                            <i class="bx bx-show"></i>
+                                                        </span>
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
     
                                     @if ($errors->register->first('password'))
@@ -107,14 +120,27 @@
     
                                 <div class="field">
                                     <label class="label" for="password_confirmation">Confirmar contraseña</label>
-                                    <div class="control has-icons-left has-icons-right">
-                                        <input class="input" type="password" name="password_confirmation" id="password_confirmation" placeholder="***********">
-                                        <span class="icon is-small is-left">
-                                            <i class="bx bx-key"></i>
-                                        </span>
-                                        <span class="icon is-small is-right">
-                                            <i class='bx bx-error-circle'></i>
-                                        </span>
+                                    <div class="field is-grouped">
+                                        <div class="field-body">
+                                            <div class="field has-addons">
+                                                <div class="control is-expanded has-icons-left has-icons-right">
+                                                    <input class="input" type="password" name="password_confirmation" id="password_confirmation" placeholder="***********">
+                                                    <span class="icon is-small is-left">
+                                                        <i class="bx bx-key"></i>
+                                                    </span>
+                                                    <span class="icon is-small is-right">
+                                                        <i class='bx bx-error-circle'></i>
+                                                    </span>
+                                                </div>
+                                                <div class="control">
+                                                    <a class="button" id="toggle-confirmation-password">
+                                                        <span class="icon is-small">
+                                                            <i class="bx bx-show"></i>
+                                                        </span>
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
     
                                     @if ($errors->register->first('password_confirmation'))
@@ -143,4 +169,38 @@
         </div>
     </div>
 
+@endsection
+
+@section('scripts')
+<script>
+    document.getElementById("toggle-password").addEventListener("click", function() {
+        var passwordInput = document.getElementById("password");
+        var eyeIcon = this.querySelector("i");
+
+        if (passwordInput.type === "password") {
+            passwordInput.type = "text";
+            eyeIcon.classList.remove("show");
+            eyeIcon.classList.add("hide");
+        } else {
+            passwordInput.type = "password";
+            eyeIcon.classList.remove("hide");
+            eyeIcon.classList.add("show");
+        }
+    });
+
+    document.getElementById("toggle-confirmation-password").addEventListener("click", function() {
+        var passwordInput = document.getElementById("password_confirmation");
+        var eyeIcon = this.querySelector("i");
+
+        if (passwordInput.type === "password") {
+            passwordInput.type = "text";
+            eyeIcon.classList.remove("show");
+            eyeIcon.classList.add("hide");
+        } else {
+            passwordInput.type = "password";
+            eyeIcon.classList.remove("hide");
+            eyeIcon.classList.add("show");
+        }
+    });
+</script>
 @endsection
